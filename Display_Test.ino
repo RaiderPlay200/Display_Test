c6adff64ec8f666114c71c991f8eb668f374c366
Matricata <mitkogeorgiev512@gmail.com>
2024-04-19 16:55:35 +0300

    Create Display_Test.ino

---
+++ Display_Test.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() 
+{
+  // put your setup code here, to run once:
+    lcd.init();
+    lcd.backlight();
+    /*
+     lcd.begin(16,2);
+     Wire.begin(); 
+     lcd.clear();
+    */
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+    lcd.setCursor(0,0);
+    lcd.print("Hello World!");
+}
